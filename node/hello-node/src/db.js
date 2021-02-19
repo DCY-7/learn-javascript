fcfe685b3e4c177854b36e7494366ff2e6de2588
邓晨阳 <747912015@qq.com>
2021-02-19 16:20:38 +0800

    测试连接MySQL数据库

--- node/hello-node/src/db.js
+++ node/hello-node/src/db.js
@@ -5,12 +5,12 @@ var connection = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: '12345678',
-    database: 'java1908z'
+    database: 'library'
 });
 
 connection.connect();
 
-connection.query('SELECT * FROM b_user',function(error,results,fields)  {
+connection.query('SELECT * FROM book',function(error,results,fields)  {
     if (error) throw error;
     console.log('[connection connect]  succeed!');
     console.log('results = ' + JSON.stringify(results));
